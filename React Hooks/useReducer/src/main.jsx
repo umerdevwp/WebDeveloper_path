ec6b65c9331ce587f38e74cd65ce1c2d529ae6f4
Umer Mushtaq <umer.cvision@gmail.com>
2024-09-20 23:52:45 +0500

    Update main.jsx
    Import reactDOM

--- React Hooks/useReducer/src/main.jsx
+++ React Hooks/useReducer/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   // Remove the React.StrictMode component to get re-renders once
   <React.StrictMode>
